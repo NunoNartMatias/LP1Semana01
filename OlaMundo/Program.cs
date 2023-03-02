6403678d761685fb5a5d4a8280318a650e875c05
NunoNartMatias <nunosilvamatias@gmail.com>
2023-03-02 22:35:27 +0000

    Added comments

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 
 namespace OlaMundo
-{
+{   
+    /// The class of our Program
     class Program
-    {
+
+    {   /// Main method 
         static void Main(string[] args)
         {
+            // Prints the corresponding text
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Bye World!");
         }
