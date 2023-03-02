d73ed78b9f7259303c869604954fc264cfb43ef8
NunoNartMatias <nunosilvamatias@gmail.com>
2023-03-02 22:27:43 +0000

    Argumentos Project added

---
+++ Argumentos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // For loop for the arguments
+            for(int i = 0; i < args.Length ; i++)
+            {
+                // Prints each argument
+                Console.WriteLine(args[1]);
+            }
+        }
+    }
+}
